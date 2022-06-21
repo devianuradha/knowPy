474095992635de673dc39bcb902a7211ee04c3c6
devianuradha <107515967+devianuradha@users.noreply.github.com>
2022-06-20 22:00:30 -0400

    Create validParentheses.py

---
+++ validParentheses.py
@@ -0,0 +1,20 @@
+#Valid Parentheses
+
+def remove_duplicate(s):
+    words=s.split()
+  #  words="".join(words)
+    print(words)
+    dup=""
+    uniq=""
+    for i in words:
+        if(i in uniq):
+            dup=dup+" "+i
+            pass
+        else:
+            uniq=uniq+" "+i
+    print("Unique Words: ",uniq)
+    print("Duplicate words: ",dup)
+    
+s="My name is Anuradha My name is not Anuradha"
+print(s)
+print(remove_duplicate(s))
