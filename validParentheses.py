9e5568994cc33905527e2a04afb392e543d06ce5
devianuradha <107515967+devianuradha@users.noreply.github.com>
2022-06-20 22:05:02 -0400

    Update validParentheses.py

--- validParentheses.py
+++ validParentheses.py
@@ -1,20 +1,26 @@
 #Valid Parentheses
+def isValid(test_str):
+  # len(test_str) is odd -> invalid!
+  if len(test_str)%2 != 0:
+    return False
+  # initialize parentheses dict
+  par_dict = {'(':')','{':'}','[':']'}
+  stack = []
+  for char in test_str:
+    # push opening bracket to stack list
+    if char in par_dict.keys():
+      stack.append(char)
+      print(stack)
+    else:
+      # closing bracket without matching opening bracket
+      if stack == []:
+          return False
+      # if closing bracket -> pop stack top
+      open_brac = stack.pop()
+      # not matching bracket -> invalid!
+      if char != par_dict[open_brac]:
+        return False
+  return stack == []
 
-def remove_duplicate(s):
-    words=s.split()
-  #  words="".join(words)
-    print(words)
-    dup=""
-    uniq=""
-    for i in words:
-        if(i in uniq):
-            dup=dup+" "+i
-            pass
-        else:
-            uniq=uniq+" "+i
-    print("Unique Words: ",uniq)
-    print("Duplicate words: ",dup)
-    
-s="My name is Anuradha My name is not Anuradha"
-print(s)
-print(remove_duplicate(s))
+str1 = '{}[{()}}'
+print(isValid(str1))
